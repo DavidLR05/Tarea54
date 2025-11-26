0029a1eae2941aa3d3ea76cd24ac0d4c9911dd1b
David Lorente Rubira <davidlorenterubira@gmail.com>
2025-11-26 19:24:14 +0100

    Implementada interfaz figura.java Fix #2

---
+++ Tarea54-DLR/src/main/java/es/gva/edu/davlorrub/tarea54/dlr/figura.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package es.gva.edu.davlorrub.tarea54.dlr;
+
+/**
+ *
+ * @author David
+ */
+public interface figura {
+    public double calcularArea();
+    public double calcularPerimetro();
+            
+    
+}
